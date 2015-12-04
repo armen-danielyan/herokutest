d5ce69ce878f429025267406c90055fbbb05e8f4
Armen <danielyan_armen@yahoo.com>
2015-12-04 18:59:48 +0400

    update

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-    echo "Hello World";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>This is my first Heroku website</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p><?php echo "Hello World"; ?></p>
+</body>
+</html>
